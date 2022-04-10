dda79db709bc88e74e1560d0f4d2f7c4eaaa65f2
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2022-04-10 22:41:53 +0530

    Fix for SH-2059, SH-2005, SH-2027, SH-1083

--- bash/status_motd_rpi.sh
+++ bash/status_motd_rpi.sh
@@ -8,19 +8,18 @@ print_line() {
     done
 }
 
-title="SSH: $(hostname)"
-echo -e "\033]2;"$title"\007"
+title="Welcome: $(hostname)"
+echo -e "\033]2;$title\007"
 
-# tput Color Table dict
-declare -A colors
-colors['RED']="tput setaf 1"
-colors['GREEN']="tput setaf 2"
-colors['YELLOW']="tput setaf 3"
-colors['BOLD']="tput bold"
-colors['RESET']="tput sgr0"
+# tput Colors
+RED="tput setaf 1"
+GREEN="tput setaf 2"
+YELLOW="tput setaf 3"
+BOLD="tput bold"
+RESET="tput sgr0"
 
 # Print Hostname
-${colors['YELLOW']}
+${YELLOW}
 
 if [ $(which toilet 2>/dev/null || false) ]; then
     toilet -F metal -f smmono12 $(hostname)
@@ -40,9 +39,9 @@ UPTIME=$(printf "%d days, %02dh %02dm %02ds" "$days" "$hours" "$mins" "$secs")
 
 # get the load averages
 read one five fifteen rest </proc/loadavg
-${colors['GREEN']}
-echo "$(date +"%A, %e %B %Y, %r")"
-${colors['RESET']}
+${GREEN}
+date +"%A, %e %B %Y, %r"
+${RESET}
 
 int() {
     expr "${1:-}" : '[^0-9]*\([0-9]*\)' 2>/dev/null || :
@@ -53,27 +52,35 @@ TEMP=$(vcgencmd measure_temp | tail -c 7)
 TEMP=${TEMP::-2}
 TEMP_STR=$(int "$TEMP")
 
-
 # OK Temperature
 if [ "$TEMP_STR" -lt 46 ]; then
-    TEMP_COLOR=${colors['GREEN']}
-    TEMP_STR="$TEMP oC (Cool \uf058)"
+    TEMPERATURE_COLOR=${GREEN}
+    TEMP_STR="$TEMP oC (Cool)"
 # Warm Temperature
 elif [ "$TEMP_STR" -lt 55 ]; then
-    TEMP_COLOR=${colors['YELLOW']}
-    TEMP_STR="$TEMP oC (Warm \uf071)"
+    TEMPERATURE_COLOR=${YELLOW}
+    TEMP_STR="$TEMP oC (Warm)"
 # Hot Temperature
 elif [ "$TEMP_STR" -gt 55 ]; then
-    TEMP_COLOR=${colors['RED']}
-    TEMP_STR="$TEMP oC (Hot \uf737)"
+    TEMPERATURE_COLOR=${RED}
+    TEMP_STR="$TEMP oC (Hot)"
 fi
 
+# Memory Used
+MEM_USED=$(free | grep Mem | awk '{print $3/1024}')
+MEM_TOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{print $2/1024}')
 
-printf "\n-$(${colors['BOLD']}) Uptime $(${colors['RESET']})            : ${UPTIME}" # skipcq: SH-2059 
-printf "\n-$(${colors['BOLD']}) Temperature $(${colors['RESET']})       : $($TEMP_COLOR)${TEMP_STR}$(${colors['RESET']})" # skipcq: SH-2059
-printf "\n-$(${colors['BOLD']}) Memory $(${colors['RESET']})            : $(free | grep Mem | awk '{print $3/1024}') MB (Used) / $(cat /proc/meminfo | grep MemTotal | awk {'print $2/1024'}) MB (Total)" # skipcq: SH-2059
-printf "\n-$(${colors['BOLD']}) Load Averages $(${colors['RESET']})     : ${one}, ${five}, ${fifteen} (1, 5, 15 min)" # skipcq: SH-2059
-printf "\n-$(${colors['BOLD']}) Running Processes $(${colors['RESET']}) : $(ps ax | wc -l | tr -d ' ')" # skipcq: SH-2059
-printf "\n-$(${colors['BOLD']}) LAN IP $(${colors['RESET']})            : $(ifconfig eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2) (eth0)" # skipcq: SH-2059
+# Processes
+RUNNING_PROC=$(ps ax | wc -l | tr -d ' ')
+
+# IP Address
+IP_ADDRESS=$(ifconfig eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+
+printf "\n-%s Uptime %s            : %s"                            "$(${BOLD})" "$(${RESET})" "${UPTIME}"
+printf "\n-%s Temperature %s       : %s%s%s"                        "$(${BOLD})" "$(${RESET})" "$($TEMPERATURE_COLOR)" "${TEMP_STR}" "$(${RESET})"
+printf "\n-%s Memory %s            : %s MB (Used) / %s MB (Total)"  "$(${BOLD})" "$(${RESET})" "${MEM_USED}" "${MEM_TOTAL}"
+printf "\n-%s Load Averages %s     : %s, %s, %s (1, 5, 15 min)"     "$(${BOLD})" "$(${RESET})" "${one}" "${five}" "${fifteen}"
+printf "\n-%s Running Processes %s : %s"                            "$(${BOLD})" "$(${RESET})" "${RUNNING_PROC}"
+printf "\n-%s LAN IP %s            : %s (eth0)"                     "$(${BOLD})" "$(${RESET})" "${IP_ADDRESS}"
 # Comment Newline if necessary
 printf "\n\n"
