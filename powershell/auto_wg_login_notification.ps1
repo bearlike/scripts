d2304bc19f8cb3421f82d2621a2252f1ee80e35a
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2022-11-17 15:45:55 -0500

    Minor Fixes
    Fixed ENV variables and string operations

--- powershell/auto_wg_login_notification.ps1
+++ powershell/auto_wg_login_notification.ps1
@@ -7,13 +7,13 @@ $networks = (get-netconnectionProfile).Name
 
 if (($home_network -In $networks) -and ($wireguard_network -In $networks)) {
     # Disconnect Wireguard, because there's no necessity for it
-    Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/uninstalltunnelservice', $wireguard_network + ".conf" -Wait -NoNewWindow -PassThru | Out-Null
+    Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/uninstalltunnelservice', "$wireguard_network.conf" -Wait -NoNewWindow -PassThru | Out-Null
 }
 
 elseif (($home_network -NotIn $networks) -and ($wireguard_network -NotIn $networks)) {
     # If Wireguard is connected or if I'm connected to my home network
-    Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/installtunnelservice', "C:\Files\configs\" + $wireguard_network + ".conf" -Wait -NoNewWindow -PassThru | Out-Null
+    Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/installtunnelservice', "C:\Files\configs\$wireguard_network.conf" -Wait -NoNewWindow -PassThru | Out-Null
 }
 
 # Send login notification via Gotify
-Start-Process "C:\Program Files\Python310\python.exe" -ArgumentList "%PROJECTS%\Automations\Scripts\python\login_notification.py" -Wait -NoNewWindow -PassThru | Out-Null
+Start-Process "C:\Program Files\Python310\python.exe" -ArgumentList "$Env:PROJECTS\Automations\Scripts\python\login_notification.py" -Wait -NoNewWindow -PassThru | Out-Null
